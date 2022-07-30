b1bb62115c768b35c6fa6ceeb9be158d74f1d31a
Angelo Rubin <angelorubin@gmail.com>
2022-07-30 15:03:48 -0300

    Implementando rota para deletar mensagem

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,20 @@
 import { Router, Request, Response } from "express";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { ListMessageController } from "./controllers/ListMessageController";
+import { DeleteMessageController } from "./controllers/DeleteMessageController";
 
 const router = Router();
 
 const createMessageController = new CreateMessageController();
 const listMessageController = new ListMessageController();
+const deleteMessageController = new DeleteMessageController();
 
-router.get("/", (request: Request, response: Response) => {
-	return response.json({ message: "Bem vindo a API Dio Shopping" });
-});
+router.get("/", (req: Request, res: Response) =>
+	res.json({ message: "Bem vindo a API do Dio Shopping" })
+);
 
 router.get("/message", listMessageController.handle);
 router.post("/message", createMessageController.handle);
+router.delete("/message/:id", deleteMessageController.handle);
 
 export { router };
