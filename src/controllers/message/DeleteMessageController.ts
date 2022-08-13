d64fd13a1682dfd15531cb883d12239a45c5db9c
Angelo Rubin <angelorubin@gmail.com>
2022-08-13 10:09:34 -0300

    Modificando nome da pasta

--- src/controllers/message/DeleteMessageController.ts
+++ src/controllers/message/DeleteMessageController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { DeleteMessageService } from "../../services/messages/DeleteMessageService";
+import { DeleteMessageService } from "../../services/message/DeleteMessageService";
 
 class DeleteMessageController {
 	async handle(req: Request, res: Response) {
